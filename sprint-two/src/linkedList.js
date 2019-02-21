a412032886e60e8ce83a38c81dcab9e8c7104fa0
M-Shehu <magnanimus.shehu@gmail.com>
2019-02-21 15:35:05 -0800

    starting linked list solution

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,9 +2,28 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.counter = 0;
 
-  list.addToTail = function(value) {
-  };
+  list.addToTail = (function() {
+    var hasHead = false;
+    return function(value) {
+      if (!hasHead) {
+        hasHead = true;
+        var newNode = new Node(value);
+        list.head = newNode;
+        list[counter] = newNode;
+        list.tail = newNode; // for first node in list
+        list.counter++;
+      } else {
+        var newNode = new Node(value);
+        var currentTail = list.tail;
+        currentTail.next = counter;
+        list.tail = newNode;
+        list[counter] = newNode; // add node to list
+        list.counter++;
+      }
+    };
+  })();
 
   list.removeHead = function() {
   };
