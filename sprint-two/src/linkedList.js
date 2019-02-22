78cfa531c1a0402307c368a6449f47dd912322a8
M-Shehu <magnanimus.shehu@gmail.com>
2019-02-22 10:40:43 -0800

    Add in time complexity analysis and comment out debug counters

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,22 +2,22 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  list.counter = 0;
+  // list.counter = 0; // not needed but for debugging purposes
 
   list.addToTail = function(value) {
     if (this.head === null) {
       var newNode = new Node(value);
       this.head = newNode;
-      this[this.counter] = newNode; // not needed but for debugging purposes
+      // this[this.counter] = newNode; 
       this.tail = newNode; // for first node in list
-      this.counter++;
+      // this.counter++;
     } else {
       var newNode = new Node(value);
       var currentTail = this.tail;
       currentTail.next = newNode;
       this.tail = newNode;
-      this[this.counter] = newNode; // add node to list
-      this.counter++;
+      // this[this.counter] = newNode; // add node to list
+      // this.counter++; 
     }
   };
 
@@ -26,6 +26,7 @@ var LinkedList = function() {
     var nextHead = this.head.next; // evaluates to null if linked list only has one value
     this.head = nextHead;
     return formerHead.value;
+
   };
 
   list.contains = function(target, node) {
@@ -53,4 +54,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  list.addToTail - O(1) : because we have pointers to head and tail  
+  list.removeHead - O(1) : because we have a pointer to head and don't need to shift other values
+  list.contains - O(n): because you need to check every node
  */
