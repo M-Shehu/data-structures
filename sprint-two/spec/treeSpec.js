968606a2eba55ca9c0f0c6b76384348441429b61
M-Shehu <magnanimus.shehu@gmail.com>
2019-02-23 18:14:53 -0800

    Add tests to tree to check for ability to add multiple children on one node

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,10 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should be able to add multiple children to one node', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    expect(tree.children.length).to.equal(2);
+  });
+
 });
